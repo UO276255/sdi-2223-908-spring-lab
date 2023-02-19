22eac8f47529925f117505584cae25b522657cf7
uo276255 <uo276255@uniovi.es>
2023-02-19 21:49:46 +0100

    Commit validacion no acabada

--- src/main/java/com/uniovi/sdi2223908spring/controller/UsersController.java
+++ src/main/java/com/uniovi/sdi2223908spring/controller/UsersController.java
@@ -2,11 +2,14 @@ package com.uniovi.sdi2223908spring.controller;
 import com.uniovi.sdi2223908spring.entities.User;
 import com.uniovi.sdi2223908spring.services.SecurityService;
 import com.uniovi.sdi2223908spring.services.UsersService;
+import com.uniovi.sdi2223908spring.validators.SignUpFormValidator;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -16,6 +19,9 @@ public class UsersController {
 
     @Autowired
     private SecurityService securityService;
+
+    @Autowired
+    private SignUpFormValidator signUpFormValidator;
 
     @RequestMapping("/user/list")
     public String getListado(Model model) {
@@ -54,12 +60,23 @@ public class UsersController {
         return "redirect:/user/details/" + id;
     }
 
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(Model model) {
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String signup(@ModelAttribute("user") User user, Model model) {
+    public String signup(@Validated User user, BindingResult result) {
+        signUpFormValidator.validate(user,result);
+        if(result.hasErrors()) {
+            return "signup";
+        }
         usersService.addUser(user);
-        securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
+        securityService.autoLogin(user.getDni(),user.getPasswordConfirm());
         return "redirect:home";
     }
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login() {
         return "login";
